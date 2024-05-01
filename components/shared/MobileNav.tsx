03113c8146e6321265f3bf458bbecc7580e10d48
Isaiah Francois <isaiah.francois@massvirtual.com>
2024-05-01 13:59:50 -0400

    Update Home component and MobileNav component

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,17 +1,16 @@
-import { SignedIn, UserButton } from '@clerk/nextjs';
+'use client';
+
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from '@/components/ui/sheet';
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { navLinks } from '@/constants';
+import { usePathname } from 'next/navigation';
+import { Button } from '../ui/button';
 
 const MobileNav = () => {
+  const pathname = usePathname();
   return (
     <header className="header">
       <Link href={'/'} className="flex items-center gap-2 md:py-2">
@@ -36,17 +35,50 @@ const MobileNav = () => {
                 className="cursor-pointer"
               />
             </SheetTrigger>
-            <SheetContent>
-              <SheetHeader>
-                <SheetTitle>Are you absolutely sure?</SheetTitle>
-                <SheetDescription>
-                  This action cannot be undone. This will permanently delete
-                  your account and remove your data from our servers.
-                </SheetDescription>
-              </SheetHeader>
+            <SheetContent className="sheet-content sm:w-64">
+              <>
+                <Image
+                  src="/assets/images/logo-text.svg"
+                  alt="logo"
+                  width={152}
+                  height={23}
+                />
+
+                <ul className="header-nav_elements">
+                  {navLinks.map((link) => {
+                    const isActive = link.route === pathname;
+
+                    return (
+                      <li
+                        key={link.route}
+                        className={`${isActive && 'gradient-text'} p-18 flex whitespace-nowrap text-dark-700`}
+                      >
+                        <Link
+                          className="sidebar-link cursor-pointer"
+                          href={link.route}
+                        >
+                          <Image
+                            src={link.icon}
+                            alt="logo"
+                            width={24}
+                            height={24}
+                          />
+                          {link.label}
+                        </Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </>
             </SheetContent>
           </Sheet>
         </SignedIn>
+
+        <SignedOut>
+          <Button asChild className="button bg-purple-gradient bg-cover">
+            <Link href="/sign-in">Login</Link>
+          </Button>
+        </SignedOut>
       </nav>
     </header>
   );
