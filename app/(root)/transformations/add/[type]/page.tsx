239094d31e4b61b2536f7610e8026659685258e0
Isaiah <isaiah.francois@massvirtual.com>
2024-05-04 21:23:44 -0400

    chore: Update Clerk webhook handling, dependencies, and layout components

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import Header from '@/components/shared/Header';
+import TransformationForm from '@/components/shared/TransformationForm';
+import { transformationTypes } from '@/constants';
 
-const AddTransformationType = () => {
-  return <div>AddTransformationType</div>;
+const AddTransformationType = ({ params: { type } }: SearchParamProps) => {
+  const transformation = transformationTypes[type];
+  return (
+    <>
+      <Header title={transformation.title} subTitle={transformation.subTitle} />
+
+      <TransformationForm />
+    </>
+  );
 };
 
 export default AddTransformationType;
