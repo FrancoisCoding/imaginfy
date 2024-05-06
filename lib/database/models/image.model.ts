442c8b845c5190a3cb998803fb665a4cffce4ef2
Isaiah <isaiah.francois@massvirtual.com>
2024-05-05 23:16:02 -0400

    refactor: Update secureURL field name in MediaUploader and image.model.ts

--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -2,9 +2,9 @@ import { Document, Schema, model, models } from 'mongoose';
 
 export interface IImage extends Document {
   title: string;
-  transformation: string;
+  transformationType: string;
   publicId: string;
-  secureUrl: URL;
+  secureURL: string;
   width?: number;
   height?: number;
   config?: object;
@@ -12,20 +12,24 @@ export interface IImage extends Document {
   aspectRatio?: string;
   color?: string;
   prompt?: string;
-  author: { _id: string; firstName: string; lastName: string };
+  author: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+  };
   createdAt?: Date;
   updatedAt?: Date;
 }
 
 const ImageSchema = new Schema({
   title: { type: String, required: true },
-  transformation: { type: String, required: true },
+  transformationType: { type: String, required: true },
   publicId: { type: String, required: true },
-  secureUrl: { type: URL, required: true },
+  secureURL: { type: String, required: true },
   width: { type: Number },
   height: { type: Number },
   config: { type: Object },
-  transformationUrl: { type: URL },
+  transformationUrl: { type: String },
   aspectRatio: { type: String },
   color: { type: String },
   prompt: { type: String },
